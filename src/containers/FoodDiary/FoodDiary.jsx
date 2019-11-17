6c5ce808dea6a810bfc7b6cb5ef90c92dc919ab0
DennieChristensen <44695505+Dennienajs@users.noreply.github.com>
2019-11-17 11:12:33 +0100

    small detail

--- src/containers/FoodDiary/FoodDiary.jsx
+++ src/containers/FoodDiary/FoodDiary.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodDiary.scss";
 import FoodDiaryHeaderDummie from "../../components/FoodDiaryHeaderDummie/FoodDiaryHeaderDummie";
 import FoodDiaryContent from "../../components/FoodDiaryContent/FoodDiaryContent";
 
 const FoodDiary = () => {
-  const date = "02-11-2019";
-  return (
-    <div className="food-diary">
-      <h1>Your food diary for: {date}</h1>
+   const [date, setDate] = useState("02-11-2019");
+   const [mealName, setMealName] = useState("Breakfast");
 
-      <div className="food-container">
-        <table className="food-table" id="food-table">
-          <FoodDiaryHeaderDummie />
-          <FoodDiaryContent date={date} />
-        </table>
+   return (
+      <div className="food-diary">
+         <h1>Your food diary for: {date}</h1>
+
+         <div className="food-container">
+            <table className="food-table" id="food-table">
+               <FoodDiaryHeaderDummie />
+               <FoodDiaryContent date={date} mealName={mealName} />
+               <FoodDiaryHeaderDummie />
+            </table>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default FoodDiary;
