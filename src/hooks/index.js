b999e4c053baf7286183b6b16ccae0ad59168ac6
Dennienajs <dennie.najs@hotmail.com>
2019-11-18 16:39:53 +0100

    unused removed

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,41 +3,29 @@ import { firebase } from "../firebase";
 
 // Custom hook -> useDiary -> henter alle diary indtastninger fra en given dato (diaryDate)
 export const useDiary = diaryDate => {
-   const [diary, setDiary] = useState([]);
+  const [diary, setDiary] = useState([]);
 
-   //NOTE: found work around - delete later when sure it works.
-   // const [breakfast, setBreakfast] = useState([]);
-   // const [lunch, setLunch] = useState([]);
-   // const [dinner, setDinner] = useState([]);
+  // unsubscribe bliver brugt. Den burde ikke brokke sig.
+  useEffect(() => {
+    let unsubscribe = firebase
+      .firestore()
+      .collection("diary")
+      .where("userId", "==", "1234567890")
+      .where("date", "==", diaryDate);
 
-   // unsubscribe bliver brugt. Den burde ikke brokke sig.
-   useEffect(() => {
-      let unsubscribe = firebase
-         .firestore()
-         .collection("diary")
-         .where("userId", "==", "1234567890")
-         .where("date", "==", diaryDate);
+    unsubscribe = unsubscribe.onSnapshot(snapshot => {
+      const diaryData = snapshot.docs.map(diarySnap => ({
+        id: diarySnap.id,
+        ...diarySnap.data()
+      }));
 
-      unsubscribe = unsubscribe.onSnapshot(snapshot => {
-         const diaryData = snapshot.docs.map(diarySnap => ({
-            id: diarySnap.id,
-            ...diarySnap.data()
-         }));
+      if (JSON.stringify(diaryData) !== JSON.stringify(diary)) {
+        if (diaryData !== undefined) {
+          setDiary(diaryData);
+        }
+      }
+    });
+  }, [diary, diaryDate]);
 
-         if (JSON.stringify(diaryData) !== JSON.stringify(diary)) {
-            if (diaryData !== undefined) {
-               setDiary(diaryData);
-               // setBreakfast(
-               //    diaryData.filter(diary => diary.mealName !== "Breakfast")
-               // );
-               // setLunch(diaryData.filter(diary => diary.mealName !== "Lunch"));
-               // setDinner(
-               //    diaryData.filter(diary => diary.mealName !== "Dinner")
-               // );
-            }
-         }
-      });
-   }, [diary, diaryDate]);
-
-   return { diary };
+  return { diary };
 };
