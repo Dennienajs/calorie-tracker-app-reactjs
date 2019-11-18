35fd925f9a5976c98c2d0def63507eaa1f253210
Dennienajs <dennie.najs@hotmail.com>
2019-11-18 11:32:42 +0100

    diary header & date styling

--- src/containers/FoodDiary/FoodDiary.jsx
+++ src/containers/FoodDiary/FoodDiary.jsx
@@ -4,22 +4,27 @@ import FoodDiaryHeaderDummie from "../../components/FoodDiaryHeaderDummie/FoodDi
 import FoodDiaryContent from "../../components/FoodDiaryContent/FoodDiaryContent";
 
 const FoodDiary = () => {
-   const [date, setDate] = useState("02-11-2019");
-   const [mealName, setMealName] = useState("Breakfast");
+  const [date, setDate] = useState("02-11-2019");
+  const [mealName, setMealName] = useState("Breakfast");
 
-   return (
-      <div className="food-diary">
-         <h1>Your food diary for: {date}</h1>
+  return (
+    <div className="food-diary">
+      <span className="food-diary-heading">
+        <h1 className="food-diary-heading__text">Your food diary for: </h1>
+        <button className="food-diary-heading__button prev">{"<--"}</button>
+        <h1 className="food-diary-heading__date">{date}</h1>
+        <button className="food-diary-heading__button next">{"-->"}</button>
+      </span>
 
-         <div className="food-container">
-            <table className="food-table" id="food-table">
-               <FoodDiaryHeaderDummie />
-               <FoodDiaryContent date={date} mealName={mealName} />
-               <FoodDiaryHeaderDummie />
-            </table>
-         </div>
+      <div className="food-diary-container">
+        <table className="food-table" id="food-table">
+          <FoodDiaryHeaderDummie />
+          <FoodDiaryContent date={date} mealName={mealName} />
+          <FoodDiaryHeaderDummie />
+        </table>
       </div>
-   );
+    </div>
+  );
 };
 
 export default FoodDiary;
