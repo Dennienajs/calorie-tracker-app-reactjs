f117eb84f50b7a187de9291afc4d503cbb3088bb
Dennienajs <dennie.najs@hotmail.com>
2019-11-18 16:40:06 +0100

    unused removed

--- src/components/Tracker/Tracker.jsx
+++ src/components/Tracker/Tracker.jsx
@@ -1,57 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Tracker.scss";
 
 const Tracker = ({ amount, size, foodText, item }) => {
-   const kcal = item.ENERC_KCAL; // total calories
-   const protein = item.PROCNT_KCAL; // protein calories
-   const fat = item.FAT_KCAL; // fat calories
-   const carbs = item.CHOCDF_KCAL; // carbohydrates calories
+  const kcal = item.ENERC_KCAL; // total calories
+  const protein = item.PROCNT_KCAL; // protein calories
+  const fat = item.FAT_KCAL; // fat calories
+  const carbs = item.CHOCDF_KCAL; // carbohydrates calories
 
-   //Overvej at smide nedenstående ind i component "Tracker" og omdøb denne fil til "TrackerList"
-   return (
-      <div className="tracker">
-         <h1>CALORIE TRACKER 101</h1>
-         <h3>{`Food: ${foodText} - ${amount}${
-            size !== "0" ? " - size: " + size : " "
-         }`}</h3>
+  //Overvej at smide nedenstående ind i component "Tracker" og omdøb denne fil til "TrackerList"
+  return (
+    <div className="tracker">
+      <h1>CALORIE TRACKER 101</h1>
+      <h3>{`Food: ${foodText} - ${amount}${
+        size !== "0" ? " - size: " + size : " "
+      }`}</h3>
 
-         <table className="tracker-search-data">
-            {typeof kcal.quantity !== "number" ? (
-               <tbody>
-                  <tr>
-                     <td>Loading...</td>
-                  </tr>
-               </tbody>
-            ) : (
-               <tbody>
-                  <tr>
-                     <td className="label">protein </td>
-                     <td>{(protein.quantity / 4).toFixed(1)} g</td>
-                  </tr>
+      <table className="tracker-search-data">
+        {typeof kcal.quantity !== "number" ? (
+          <tbody>
+            <tr>
+              <td>Loading...</td>
+            </tr>
+          </tbody>
+        ) : (
+          <tbody>
+            <tr>
+              <td className="label">protein </td>
+              <td>{(protein.quantity / 4).toFixed(1)} g</td>
+            </tr>
 
-                  <tr>
-                     <td className="label">fat </td>
-                     <td>{(fat.quantity / 9).toFixed(1)} g</td>
-                  </tr>
+            <tr>
+              <td className="label">fat </td>
+              <td>{(fat.quantity / 9).toFixed(1)} g</td>
+            </tr>
 
-                  <tr>
-                     <td className="label">carbs </td>
-                     <td>{(carbs.quantity / 4).toFixed(1)} g</td>
-                  </tr>
+            <tr>
+              <td className="label">carbs </td>
+              <td>{(carbs.quantity / 4).toFixed(1)} g</td>
+            </tr>
 
-                  <tr>
-                     <td className="label">Total</td>
+            <tr>
+              <td className="label">Total</td>
 
-                     <td>
-                        {kcal.quantity} {kcal.unit}
-                     </td>
-                  </tr>
-                  {console.log("item.kcal" + kcal.quantity)}
-               </tbody>
-            )}
-         </table>
-      </div>
-   );
+              <td>
+                {kcal.quantity} {kcal.unit}
+              </td>
+            </tr>
+            {console.log("item.kcal" + kcal.quantity)}
+          </tbody>
+        )}
+      </table>
+    </div>
+  );
 };
 
 export default Tracker;
