2d49d5ef17354198e19aa992c60b8fd0adc11895
Dennienajs <dennie.najs@hotmail.com>
2019-11-19 18:59:34 +0100

    added daily totals

--- src/components/FoodDiaryContent/FoodDiaryContent.jsx
+++ src/components/FoodDiaryContent/FoodDiaryContent.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./FoodDiaryContent.scss";
 import FoodDiaryRenderMeals from "./FoodDiaryRenderMeals";
+import FoodDiaryRenderDailyTotal from "./FoodDiaryRenderDailyTotal";
 
 const FoodDiaryContent = ({ date, mealName }) => {
+  // Renders all the meals by mapping through.
   return (
-    <tbody className="food-diary-content">
-      {mealName.map((meal, index) => (
-        <FoodDiaryRenderMeals date={date} mealName={meal} key={index} />
-      ))}
-    </tbody>
+    <>
+      <tbody className="food-diary-content">
+        {mealName.map(meal => (
+          <FoodDiaryRenderMeals date={date} mealName={meal} key={meal} />
+        ))}
+      </tbody>
+      <tfoot className="food-diary-totals">
+        <FoodDiaryRenderDailyTotal
+          date={date}
+          mealName={mealName}
+          key={mealName}
+        />
+      </tfoot>
+    </>
   );
 };
 
